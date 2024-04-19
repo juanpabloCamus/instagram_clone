1e77b0443286d09531cfb7c575565238cbb4865d
Juan Pablo Camus <juanpablo@meowtel.com>
2024-04-19 09:53:53 -0300

    initial setup

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > webScreenSize) {
+            // Web Screen
+            return webScreenLayout;
+          }
+          // Mobile Screen
+          return mobileScreenLayout;
+        },
+      ),
+    );
+  }
+}
