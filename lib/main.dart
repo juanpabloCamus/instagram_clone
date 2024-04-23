a2064d3e8c4a9be304c382d686518008d566d66b
Juan Pablo Camus <juanpablo@meowtel.com>
2024-04-23 18:21:15 -0300

    setup web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -8,7 +9,17 @@ import 'package:instagram_clone/utils/colors.dart';
 void main() async {
   // Initialize Firebase
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyCwsxlgaP8YpkgWv0lYnUHdEuvTbbDIk9s',
+          appId: '1:560459008627:web:d75293422defedf574b806',
+          messagingSenderId: '560459008627',
+          projectId: 'instagram-18878'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
 
   //Init App
   runApp(const MyApp());
