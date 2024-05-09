1fad104e7868d8662e863d905f441b4f7317e015
Juan Pablo Camus <juanpablo@meowtel.com>
2024-05-09 11:14:01 -0300

    add search

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_sceen.dart';
+import 'package:instagram_clone/screens/search_screen.dart';
 
 const int webScreenSize = 600;
 
 const List<Widget> homeScreenItems = <Widget>[
   FeedScreen(),
-  Text('search'),
+  SearchScreen(),
   AddPostScreen(),
   Text('fav'),
   Text('prof'),
